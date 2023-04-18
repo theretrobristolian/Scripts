16e75e18110737a4d8b1a34825f6f2017719e838
Dave - TheRetroBristolian <106443135+theretrobristolian@users.noreply.github.com>
2023-04-18 09:48:30 +0100

    Initial Commit

---
+++ Deskew/Deskew.ps1
@@ -0,0 +1,17 @@
+﻿cls
+### Variables
+$deskew64 = "C:\IT\Deskew\Bin\deskew.exe"
+$files = "C:\IT\extracted\1"
+$deskewed = "C:\IT\extracted\deskewed"
+
+foreach ($file in Get-ChildItem -Path $files -Filter *.tif) {
+    ### Variables
+    $Document = $($file.FullName)
+    $Name = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+
+    ###Log
+    Write-Host "Now Deskewing $($file.Name)..."
+
+    ### Command
+    & "$deskew64" -t a -a 10 -b FFFFFF -c tlzw -o "$deskewed\$($file.Name)" "$Document" | Out-Null
+    }
---
+++ TIF-Converter/TIF-Converter.ps1
@@ -0,0 +1,18 @@
+﻿cls
+### Variables
+$IrfanView64 = "C:\Program Files\IrfanView\i_view64.exe"
+$files = "C:\IT\JPT"
+$extracted = "C:\IT\extracted"
+
+foreach ($file in Get-ChildItem -Path $files -Filter *.tif) {
+    ### Variables
+    $Document = $($file.FullName)
+    $Name = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+
+    ###Log
+    Write-Host "Now Extracting..."
+    Write-Host "$($file.Name)"
+
+    ### Command
+    & 'cmd.exe' /c "`"$IrfanView64`" `"$Document`" /extract=`"($extracted\$Name,tif)`" /cmdexit"
+    }
