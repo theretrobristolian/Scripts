d0e2079da2ad5af48b9d2687feac33873e1dd33c
Dave - TheRetroBristolian <106443135+theretrobristolian@users.noreply.github.com>
2023-10-30 15:50:22 +0000

    Update

--- Audio-Normalization/Normalize-Audio.ps1
+++ Audio-Normalization/Normalize-Audio.ps1
@@ -1,13 +1,57 @@
-# Set the path to the FFmpeg executable
-$ffmpegPath = "C:\path\to\ffmpeg.exe"
+# This script is a personal project that normalizes the audio levels of videos in two distinct source folders from multiple original sources.
 
-# Set the source and output directories
-$sourceDir = "C:\path\to\source"
-$outputDir = "C:\path\to\output"
+# Clear the console
+Clear-Host
+
+Write-Output "******** Normalize Audio Levels *********"
+Write-Output "************ Version 1.0 *************"
+Write-Output "******* Last updated 30/10/2023 ********"
+Write-Output ""
+
+### Variables
+$baseDirectory = "C:\Scripts\Normalize" # Define the base directory
+$subdirectories = @("Music_Videos", "Trailers_Adverts") # Define the subdirectories
+$appsDirectory = "C:\Scripts\Apps" # Define the 'Apps' directory
+
+# Automatically set the script path to the location the script is running from (useful for relative paths)
+$ScriptRoot = $PSScriptRoot
+
+# Set the path to the FFmpeg executable
+$ffmpegPath = Join-Path -Path (Join-Path -Path $appsDirectory -ChildPath "FFmpeg") -ChildPath "ffmpeg.exe"
 
 # Set the target LRA levels
 $lraMusicVideo = -14
 $lraAdvert = -7
+
+# Check and create the 'Apps' directory if it doesn't exist
+if (-not (Test-Path -Path $appsDirectory -PathType Container)) {
+    New-Item -Path $appsDirectory -ItemType Directory | Out-Null
+}
+
+# Check and create the base directory if it doesn't exist
+if (-not (Test-Path -Path $baseDirectory -PathType Container)) {
+    New-Item -Path $baseDirectory -ItemType Directory | Out-Null
+}
+
+# Create the source and modified directories
+$subdirectories | ForEach-Object {
+    $sourcePath = Join-Path -Path $baseDirectory -ChildPath "Source\$_"
+    $modifiedPath = Join-Path -Path $baseDirectory -ChildPath "Modified\$_"
+    
+    if (-not (Test-Path -Path $sourcePath -PathType Container)) {
+        New-Item -Path $sourcePath -ItemType Directory | Out-Null
+    }
+    
+    if (-not (Test-Path -Path $modifiedPath -PathType Container)) {
+        New-Item -Path $modifiedPath -ItemType Directory | Out-Null
+    }
+}
+
+# Set the source and output directories
+$sourceDir = Join-Path -Path $baseDirectory -ChildPath "Source"
+$outputDir = Join-Path -Path $baseDirectory -ChildPath "Modified"
+
+### Functions
 
 # Function to process and normalize videos
 function Process-Videos {
@@ -18,13 +62,19 @@ function Process-Videos {
     )
     
     Get-ChildItem -Path $sourceFolder -Filter *.mp4 | ForEach-Object {
-        $outputFile = Join-Path $outputFolder $_.Name
+        $outputFile = Join-Path -Path $outputFolder -ChildPath $_.Name
         & $ffmpegPath -i $_.FullName -af "loudnorm=I=$lraTarget:LRA=11:TP=-2" -c:v copy $outputFile
     }
 }
 
+### MAIN LOOP
+
+# Uncomment and use the following lines to process videos
+
 # Process music videos
-Process-Videos -sourceFolder "$sourceDir\music_videos" -outputFolder "$outputDir\music_videos" -lraTarget $lraMusicVideo
+#Process-Videos -sourceFolder (Join-Path -Path $sourceDir -ChildPath "Music_Videos") -outputFolder (Join-Path -Path $outputDir -ChildPath "Music_Videos") -lraTarget $lraMusicVideo
 
 # Process adverts
-Process-Videos -sourceFolder "$sourceDir\adverts" -outputFolder "$outputDir\adverts" -lraTarget $lraAdvert
+#Process-Videos -sourceFolder (Join-Path -Path $sourceDir -ChildPath "Trailers_Adverts") -outputFolder (Join-Path -Path $outputDir -ChildPath "Trailers_Adverts") -lraTarget $lraAdvert
+
+# End of the script
--- Audio-Normalization/README.md
+++ Audio-Normalization/README.md
@@ -1,2 +1,33 @@
-# Scripts
-This guide will explain how to normalize video audio levels
+# Audio Normalization Script for Video Looping on Raspberry Pi
+
+This `Normalize-Audio.ps1` script is designed to simplify the process of normalizing audio levels in video content. It works with two distinct sources of video files: music videos and trailers/advertisements, and outputs them to separate folders for use in a video looping system on a Raspberry Pi.
+
+## Prerequisites
+
+Before using this script, ensure you have the following prerequisites in place:
+
+### System Requirements
+
+- **Operating System**: Windows 7 or later
+
+### Software Dependencies
+
+1. **7-Zip**: Install 7-Zip on your system. You can download it from [here](https://www.7-zip.org/download.html).
+
+2. **FFmpeg**: Install FFmpeg on your system. You can download it from [here](https://ffmpeg.org/download.html#build-windows).
+
+3. **PowerShell**: Have PowerShell installed and basic knowledge of its usage.
+
+### Script and Folder Structure
+
+- Download the script from this Git repository.
+- The script can help you create a basic folder structure on the first run.
+
+## Getting Started
+
+1. Download the script from this repository.
+2. Ensure you meet all the prerequisites listed above.
+3. Run the script to do a first run and create the folder structure required.
+4. using 7-zip extract the ffmpeg download and place the bin folder inin C:\Scripts\Apps\ and rename it to FFmpeg.
+
+Now you're ready to use the `Normalize-Audio.ps1` script to prepare your videos for your Raspberry Pi video looping system. Enjoy a seamless and consistent audio experience!
