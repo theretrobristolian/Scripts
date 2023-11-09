c6554eec00ef9b50d7c4552efbfda4b10ac8dee9
Dave - TheRetroBristolian <106443135+theretrobristolian@users.noreply.github.com>
2023-11-09 19:51:37 +0000

    Update

---
+++ PS-Vita/PSPtoSD.ps1
@@ -0,0 +1,11 @@
+﻿cls
+
+# Set your source and destination paths
+$sourcePath = "\\retronas\retronas\ps3\ps3netsrv\PSPISO"
+$destinationPath = "D:\pspemu\ISO"
+
+# Define Robocopy options for actual copy without deleting extras
+$robocopyOptions = @("/E", "/ZB", "/DCOPY:T", "/COPY:DAT", "/R:5", "/W:5", "/V", "/TEE", "/NP", "/XX", "*.iso")
+
+# Run the Robocopy command and capture the output
+& robocopy $sourcePath $destinationPath $robocopyOptions
